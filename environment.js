0d08671e78c18b018646c2fd4d799bf8c1707807
mak4026 <chikuwa2135@gmail.com>
2018-08-13 04:30:41 +0900

    move glicko settings to config.json

--- environment.js
+++ environment.js
@@ -1,18 +1,7 @@
 var glicko2 = require('glicko2');
 var Player = require('./player');
 
-const settings = {
-  // tau : "Reasonable choices are between 0.3 and 1.2, though the system should
-  //       be tested to decide which value results in greatest predictive accuracy."
-  tau : 0.5,
-  // rating : default rating
-  rating : 2100,
-  //rd : Default rating deviation
-  //     small number = good confidence on the rating accuracy
-  rd : 200,
-  //vol : Default volatility (expected fluctation on the player rating)
-  vol : 0.06
-};
+const settings = require('./config.json').glicko_setting;
 
 var Environment = function() {
   this.gameCount = 0;
